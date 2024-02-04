79a66ee33044c528b3a823a994b5c6de9b764ebc
Muhammad Raihan <muhammadraihan118@gmail.com>
2024-02-04 17:55:05 +0700

    add active menu style

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -9,8 +9,11 @@ import { FaClockRotateLeft } from "react-icons/fa6";
 import { FaClock } from "react-icons/fa6";
 import { MdOutlineEventNote } from "react-icons/md";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
     <nav className="p-4">
       <Flex
@@ -22,32 +25,40 @@ const Navbar = () => {
         <Text fontWeight={"bold"} fontSize={"x-large"}>
           Boxboard
         </Text>
-        <Flex columnGap={10}>
-          <Link href={"/"} data-testid="menu-item">
+        <Flex columnGap={10} alignItems={"center"}>
+          <Link
+            href={"/"}
+            data-testid="menu-item"
+            className={router.pathname === "/" ? "menu--active" : ""}
+          >
             <Flex alignItems={"center"} columnGap={1}>
               <PiSquaresFourLight color="#BBC3CC" />
               <Text color="thirdgray">Overview</Text>
             </Flex>
           </Link>
-          <Link href={"/jobs"} data-testid="menu-item">
+          <Link
+            href={"/jobs"}
+            data-testid="menu-item"
+            className={router.pathname === "/jobs" ? "menu--active" : ""}
+          >
             <Flex alignItems={"center"} columnGap={1}>
               <FiSearch color="#BBC3CC" />
               <Text color="thirdgray">Job Search</Text>
             </Flex>
           </Link>
-          <Link href={"/"} data-testid="menu-item">
+          <Link href={"/schedule"} data-testid="menu-item">
             <Flex alignItems={"center"} columnGap={1}>
               <FaClockRotateLeft color="#BBC3CC" />
-              <Text color="thirdgray">Schdule</Text>
+              <Text color="thirdgray">Schedule</Text>
             </Flex>
           </Link>
-          <Link href={"/"} data-testid="menu-item">
+          <Link href={"/histor"} data-testid="menu-item">
             <Flex alignItems={"center"} columnGap={1}>
               <FaClock color="#BBC3CC" />
               <Text color="thirdgray">History</Text>
             </Flex>
           </Link>
-          <Link href={"/"} data-testid="menu-item">
+          <Link href={"/events"} data-testid="menu-item">
             <Flex alignItems={"center"} columnGap={1}>
               <MdOutlineEventNote color="#BBC3CC" />
               <Text color="thirdgray">Events</Text>
